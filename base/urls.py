8eff1c43e43b2cea8b0537c8aeca033465f96d76
BloodySweet23 <harisankarsunil2001@gmail.com>
2023-07-24 05:20:14 +0530

    message

---
+++ base/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views 
+urlpatterns = [
+    path('login/',views.loginPage, name="login"),
+    path('register/',views.registerPage, name="register"),
+    path('profile/<str:pk>/',views.userProfile, name="user-profile"),
+    path('logout/',views.logoutPage, name="logout"),
+    path('',views.home, name="home"),
+    path('room/<str:pk>/',views.room, name="room"),
+    path('create-room/',views.CreateRoom, name="Create_room"),
+    path('update-room/<str:pk>',views.updateRoom, name="update_room"),
+    path('delete-room/<str:pk>',views.deleteRoom, name="delete_room"),
+    path('delete-msg/<str:pk>',views.deleteMessage, name="delete_msg"),
+]
